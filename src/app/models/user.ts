916cb9a4bfe1a9e81332692f3d8ad15ca73cc463
Stavroula Bakogianni <stauroulabak@gmail.com>
2024-09-29 22:34:14 +0300

    Create User Interface.

---
+++ src/app/models/user.ts
@@ -0,0 +1,14 @@
+import { Role } from "./role.enum";
+
+export interface User {
+    id?: number;
+    vat: string;
+    name: string;
+    surname: string;
+    address?: string;
+    phoneNumber?: string;
+    email: string;
+    password: string;
+    isDeleted?: boolean;
+    role: Role;
+}
