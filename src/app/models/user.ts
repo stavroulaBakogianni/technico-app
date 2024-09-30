f9acc9385993b5daf1b4a64494824e2bb808b54d
Stavroula Bakogianni <stauroulabak@gmail.com>
2024-09-30 22:11:40 +0300

    Update user.ts

--- src/app/models/user.ts
+++ src/app/models/user.ts
@@ -1,3 +1,4 @@
+import { Property } from "./property";
 import { Role } from "./role.enum";
 
 export interface User {
@@ -11,4 +12,5 @@ export interface User {
     password: string;
     isDeleted?: boolean;
     role: Role;
+    properties?:Property[];
 }
