ce83bfd0269d048253f5d7d9f5121b23f6b8ea3f
Stavroula Bakogianni <stauroulabak@gmail.com>
2024-10-01 23:18:16 +0300

    Create routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,5 @@
 import { Routes } from '@angular/router';
+import { RegisterComponent } from './register/register.component';
+import { UserComponent } from './user/user.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [{ path: 'register', component: RegisterComponent },{path: 'home', component: UserComponent}];
