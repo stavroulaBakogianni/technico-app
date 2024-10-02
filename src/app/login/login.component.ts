62beafe8cf7e0d277791ab52a0e8a9ae67ad238a
Stavroula Bakogianni <stauroulabak@gmail.com>
2024-10-02 22:54:27 +0300

    Add login component

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,47 @@
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router, RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { UserService } from '../services/userService';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [RouterOutlet, RouterLink, RouterLinkActive, CommonModule, ReactiveFormsModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+export class LoginComponent {
+  fb = inject(FormBuilder);
+  router = inject(Router);
+  userService = inject(UserService);
+
+  loginForm: FormGroup;
+
+  constructor() {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(8)]]
+    });
+  }
+
+  onLogin() {
+    if (this.loginForm.valid) {
+      const { email, password } = this.loginForm.value;
+      this.userService.getUserByEmailAndPassword(email, password).subscribe({
+        next: (user) => {
+          this.userService.setCurrentUser(user);
+          console.log('Login successful', user);
+          if (user.role === "PROPERTY_OWNER") {
+            this.router.navigate(['/home']);
+          }
+          else { this.router.navigate(['/users']); }
+        },
+        error: (err) => {
+          console.error('Login failed:', err);
+        }
+      });
+    }
+  }
+
+}
