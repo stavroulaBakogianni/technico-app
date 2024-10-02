df5bdf262dbd327c8d7612be0a3906dec4967f10
Stavroula Bakogianni <stauroulabak@gmail.com>
2024-10-02 22:56:57 +0300

    Add path: "users"

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,6 @@
 import { Routes } from '@angular/router';
 import { RegisterComponent } from './register/register.component';
+import { HomeComponent } from './home/home.component';
 import { UserComponent } from './user/user.component';
 
-export const routes: Routes = [{ path: 'register', component: RegisterComponent },{path: 'home', component: UserComponent}];
+export const routes: Routes = [{ path: 'register', component: RegisterComponent },{path: 'home', component: HomeComponent},{path: "users", component: UserComponent }];
