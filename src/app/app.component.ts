c137abaff3d6dee273c4bc2ad972e63971a7df50
Stavroula Bakogianni <stauroulabak@gmail.com>
2024-10-02 22:57:13 +0300

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,23 @@
 import { Component } from '@angular/core';
-import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { RouterLink, RouterLinkActive, RouterOutlet, Router } from '@angular/router';
 import { UserComponent } from "./user/user.component";
 import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from "./login/login.component";
+import { CommonModule } from '@angular/common';
+import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterLink, RouterLinkActive, UserComponent, RegisterComponent],
+  imports: [RouterOutlet, UserComponent, RegisterComponent, LoginComponent, CommonModule, HomeComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'technico-app';
+  public router: Router; 
+
+  constructor(router: Router) {
+    this.router = router;
+  }
 }
